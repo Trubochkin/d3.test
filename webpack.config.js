b5cbd48677c591d14bdceae849a63a0aabeda09c
Trubochkin <lexich1990@gmail.com>
2017-09-04 21:17:54 +0300

    config marge

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,38 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const devserver = require('./webpack/devserver');
+
 const PATHS = {
     source: path.join(__dirname, 'source'),
     build: path.join(__dirname, 'build')
 };
 
-const common = {
-    entry: PATHS.source + "/index.js",
-    output: {
-        path: PATHS.build,
-        filename: "[name].js"
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            chunks: ['index'],
-            template: PATHS.source + '/index.html'
-        })
-    ]
-};
-
-const developmentConfig = {
-    devServer: {
-        inline:true,
-        port: 3000,
-        stats: 'errors-only'
+const common = merge([
+    {
+        entry: PATHS.source + "/index.js",
+        output: {
+            path: PATHS.build,
+            filename: "[name].js"
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                filename: 'index.html',
+                chunks: ['index'],
+                template: PATHS.source + '/index.html'
+            })
+        ]
     }
-};
+]);
+
+
 
 module.exports = (env) => {
     if(env === 'dev') {
-        return Object.assign(
-            {},
+        return merge([
             common,
-            developmentConfig
-        );
+            devserver()
+        ]);
     }
     if(env === 'prod'){
         return common;
